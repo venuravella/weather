ebfca293e75225f6cfa44377e2a5f82868b34f3d
venuravella <venu.ravella@clairvoyantsoft.com>
2021-09-02 14:51:33 +0530

    added slash to url

--- src/main/java/com/pearson/weather/constant/WeatherUrlConstants.java
+++ src/main/java/com/pearson/weather/constant/WeatherUrlConstants.java
@@ -3,16 +3,17 @@ package com.pearson.weather.constant;
 
 public interface WeatherUrlConstants {
 
+  String slash = "/";
   String addCity = "add/{cityName}/{countryName}/{city}";
   String deleteCity = "delete/{cityName}";
   String getAllCity = "showAll";
   String fetchCity = "show/{cityName}/{countryName}";
 
   public enum UrlConstants {
-    ADD_CITY(addCity),
-    DELETE_CITY(deleteCity),
-    GET_CITIES(getAllCity),
-    FETCH_CITY(fetchCity);
+    ADD_CITY(slash + addCity),
+    DELETE_CITY(slash + deleteCity),
+    GET_CITIES(slash + getAllCity),
+    FETCH_CITY(slash + fetchCity);
 
     private final String value;
 
